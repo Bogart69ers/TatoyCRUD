88407d1002e7533e67980b9e37ffb72413590e12
Bogart <146941786+Bogart69ers@users.noreply.github.com>
2024-02-16 21:40:44 +0800

    Update Final hehe

--- TatoyCRUD/Controllers/HomeController.cs
+++ TatoyCRUD/Controllers/HomeController.cs
@@ -11,7 +11,12 @@ namespace TatoyCRUD.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var list = new List<User>();
+            using (var db = new CRUDEntities())
+            {
+                list = db.User.ToList();
+            }
+                return View(list);
         }
 
         public ActionResult Create()
@@ -19,14 +24,62 @@ namespace TatoyCRUD.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Create(String Username, String Password)
+        public ActionResult Create(User u)
         {
-            return View();
+            using (var db = new CRUDEntities())
+            {
+                var newUser = new User();
+                newUser.username = u.username;
+                newUser.password = u.password;
+
+                db.User.Add(newUser);
+                db.SaveChanges();
+
+                TempData["msg"] = $"Added {newUser.username} Successfuly!";
+
+            }
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Update(int id)
+        {
+            var u = new User();
+            using (var db = new CRUDEntities())
+            {
+                u = db.User.Find(id);
+            }
+                return View(u);
+        }
+        [HttpPost]
+        public ActionResult Update(User u)
+        {
+            using (var db = new CRUDEntities())
+            {
+                var newUser = db.User.Find(u.id);
+                newUser.username = u.username;
+                newUser.password = u.password;
+               
+                db.SaveChanges();
+
+                TempData["msg"] = $"Updated {newUser.username} Successfuly!";
+
+            }
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Remove(int id) 
+        {
+            var u = new User();
+            using (var db = new CRUDEntities())
+            {
+                u = db.User.Find(id);
+                db.User.Remove(u);
+                db.SaveChanges();
+
+                TempData["msg"] = $"Deleted {u.username} Succesfuly!";
+            }
+            return RedirectToAction("Index");
         }
 
-        public ActionResult Remove()
-        {
-            return View();
-        }
     }
 }
