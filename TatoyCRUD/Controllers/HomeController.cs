470153371455dffd96500d4786ea2c479edc1b6b
Bogart <146941786+Bogart69ers@users.noreply.github.com>
2024-02-16 19:55:16 +0800

    Add project files.

---
+++ TatoyCRUD/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TatoyCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(String Username, String Password)
+        {
+            return View();
+        }
+
+        public ActionResult Remove()
+        {
+            return View();
+        }
+    }
+}
